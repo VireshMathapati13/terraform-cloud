d9028a3534711e69d31018ece83fe4e4c8c369b8
Viresh Mathapati <vireshmathapati13@gmail.com>
2023-08-29 23:47:21 +0530

    Create main.tf

---
+++ test-rg/main.tf
@@ -0,0 +1,20 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.91.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "test-rg1" {
+  name     = "viresh-test-rg1"
+  location = "eastus"
+
+}
